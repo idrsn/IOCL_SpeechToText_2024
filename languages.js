c6558a0e4e5b19115eb0cdcc96c80bb837addd49
Darshan Nath <nathdarshan000@gmail.com>
2024-01-10 20:20:32 +0530

    IOCL winter intern

---
+++ languages.js
@@ -0,0 +1,14 @@
+const languages = [
+  {
+    no: "16",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {
+    no: "27",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },
+];
